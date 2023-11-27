2ce2feff2e26294eb5ddf9d278780482963bc8a7
Nikita Astaforkin <astaforkin90@gmail.com>
2023-11-27 23:00:42 +0300

    first commit

--- api_yamdb/reviews/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils import timezone
-
-
-def validate_year(value):
-    if timezone.now().year < value:
-        raise ValidationError(
-            "The year of publication cannot be greater than the current year"
-        )
